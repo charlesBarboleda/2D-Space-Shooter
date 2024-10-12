46130af9a46f35e1a709d19a8ad39ed9ea3d6440
charlesBarboleda <166056415+charlesBarboleda@users.noreply.github.com>
2024-10-12 11:13:34 -0700

    Added the ability to rebake nav mesh surface with a call of a method

---
+++ Assets/NavMeshScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using NavMeshPlus.Components;
+using UnityEngine;
+
+public class NavMeshScript : MonoBehaviour
+{
+    public static NavMeshScript Instance { get; private set; }
+    public NavMeshSurface navMeshSurface;
+
+    void Start()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    // Call this method to update the NavMesh
+    public void UpdateBuildNavMesh()
+    {
+        navMeshSurface.UpdateNavMesh(navMeshSurface.navMeshData);
+    }
+
+
+}
