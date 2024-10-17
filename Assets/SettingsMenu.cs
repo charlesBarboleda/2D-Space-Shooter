db0c9d344f820b145542ccf23d7fd4aa63f6d887
charlesBarboleda <166056415+charlesBarboleda@users.noreply.github.com>
2024-10-16 18:48:13 -0700

    Implemented a graphics quality drop down setting for the settings menu

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -11,4 +11,9 @@ public class SettingsMenu : MonoBehaviour
     {
         audioMixer.SetFloat("volume", volume);
     }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
 }
